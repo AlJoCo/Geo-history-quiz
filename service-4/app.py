dc6d1b53eed3b611e417a8746557a8393ddd187f
alex <alexanderjcoates@gmail.com>
2021-06-08 12:48:31 +0100

    midday-two

--- service-4/app.py
+++ service-4/app.py
@@ -1,6 +1,8 @@
 import requests
 from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import BooleanField
 from os import getenv
 
 app = Flask(__name__)
@@ -19,3 +21,15 @@ class Storage(db.Model):
     #x axis
     Date = db.Column(db.String(50), nullable=False)
     Survivable = db.Column(db.Boolean)
+
+class form(FlaskForm):
+    Survivable = BooleanField("Is this survivable?")
+
+def newcharacter():
+    form = form()
+    queryall = Storage.query.all()
+    if form.validate_on_submit():
+        new_entry = Storage(longitude = y.service-2.data)
+        db.session.add(new_entry)
+        db.session.commit()
+    return render_template('newcharacter.html', queryall=queryall, form=form)
