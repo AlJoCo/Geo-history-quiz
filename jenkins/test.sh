159f6bbe56eb179935fa6b938f87308148c5005c
Alex <ACoates@academytrainee.com>
2021-06-11 08:37:34 +0000

    jenkins tweaking

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install python3-venv python3-pip -y
+sudo apt-get install python3-pip3 python3-venv -y
 
 python3 -m venv venv
 #virtualenv -p python3 venv
-source venv/bin/activate
+source ./venv/bin/activate
 
 pip3 install -r test_requirements.txt
 
